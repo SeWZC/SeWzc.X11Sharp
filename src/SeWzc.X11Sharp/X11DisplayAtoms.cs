132131b101c8ec448b5fd9a4235ef3eef4e554bc
SeWZC <36623158+SeWZC@users.noreply.github.com>
2025-08-01 01:24:03 +0800

    补充 Extended Window Manager Hints 原子

--- src/SeWzc.X11Sharp/X11DisplayAtoms.cs
+++ src/SeWzc.X11Sharp/X11DisplayAtoms.cs
@@ -1,4 +1,7 @@
-﻿namespace SeWzc.X11Sharp;
+﻿using System.Collections.Concurrent;
+using SeWzc.X11Sharp.Internal;
+
+namespace SeWzc.X11Sharp;
 
 /// <summary>
 /// Display 中的原子。
@@ -6,6 +9,7 @@
 public class X11DisplayAtoms
 {
     private readonly X11Display _display;
+    private readonly ConcurrentDictionary<string, X11Atom> _atomCache = new();
 
     internal X11DisplayAtoms(X11Display display)
     {
@@ -14,37 +18,550 @@ public class X11DisplayAtoms
 
     #region 扩展原子
 
-    private X11Atom _utf8String;
-
     /// <summary>
     /// UTF8_STRING 原子。
     /// </summary>
-    public X11DisplayAtom Utf8String => GetAtom(ref _utf8String, "UTF8_STRING");
+    public X11DisplayAtom Utf8String => GetAtom("UTF8_STRING");
 
-    private X11Atom _netWmName;
+    #region Extended Window Manager Hints
+
+    // 基于 Extended Window Manager Hints (EWMH) 的原子。
+    // https://specifications.freedesktop.org/wm-spec/1.5/index.html
+
+    #region Root Window Properties
+
+    /// <summary>
+    /// _NET_SUPPORTED 原子。
+    /// </summary>
+    /// <remarks>
+    /// _NET_SUPPORTED, ATOM[]/32
+    /// </remarks>
+    public X11DisplayAtom NetSupported => GetAtom("_NET_SUPPORTED");
+
+    /// <summary>
+    /// _NET_CLIENT_LIST 原子。
+    /// </summary>
+    /// <remarks>
+    /// _NET_CLIENT_LIST, WINDOW[]/32
+    /// </remarks>
+    public X11DisplayAtom NetClientList => GetAtom("_NET_CLIENT_LIST");
+
+    /// <summary>
+    /// _NET_CLIENT_LIST_STACKING 原子。
+    /// </summary>
+    /// <remarks>
+    /// _NET_CLIENT_LIST_STACKING, WINDOW[]/32
+    /// </remarks>
+    public X11DisplayAtom NetClientListStacking => GetAtom("_NET_CLIENT_LIST_STACKING");
+
+    /// <summary>
+    /// _NET_NUMBER_OF_DESKTOPS 原子。
+    /// </summary>
+    /// <remarks>
+    /// _NET_NUMBER_OF_DESKTOPS, CARDINAL/32
+    /// </remarks>
+    public X11DisplayAtom NetNumberOfDesktops => GetAtom("_NET_NUMBER_OF_DESKTOPS");
+
+    /// <summary>
+    /// _NET_CURRENT_DESKTOP 原子。
+    /// </summary>
+    /// <remarks>
+    /// _NET_DESKTOP_GEOMETRY width, height, CARDINAL[2]/32
+    /// </remarks>
+    public X11DisplayAtom NetDesktopGeometry => GetAtom("_NET_DESKTOP_GEOMETRY");
+
+    /// <summary>
+    /// _NET_DESKTOP_VIEWPORT 原子。
+    /// </summary>
+    /// <remarks>
+    /// _NET_DESKTOP_VIEWPORT x, y, CARDINAL[][2]/32
+    /// </remarks>
+    public X11DisplayAtom NetDesktopViewport => GetAtom("_NET_DESKTOP_VIEWPORT");
+
+    /// <summary>
+    /// _NET_CURRENT_DESKTOP 原子。
+    /// </summary>
+    /// <remarks>
+    /// _NET_CURRENT_DESKTOP desktop, CARDINAL/32
+    /// </remarks>
+    public X11DisplayAtom NetCurrentDesktop => GetAtom("_NET_CURRENT_DESKTOP");
+
+    /// <summary>
+    /// _NET_DESKTOP_NAMES 原子.
+    /// </summary>
+    /// <remarks>
+    /// _NET_DESKTOP_NAMES, UTF8_STRING[]
+    /// </remarks>
+    public X11DisplayAtom NetDesktopNames => GetAtom("_NET_DESKTOP_NAMES");
+
+    /// <summary>
+    /// _NET_ACTIVE_WINDOW 原子。
+    /// </summary>
+    /// <remarks>
+    /// _NET_ACTIVE_WINDOW, WINDOW/32
+    /// </remarks>
+    public X11DisplayAtom NetActiveWindow => GetAtom("_NET_ACTIVE_WINDOW");
+
+    /// <summary>
+    /// _NET_WORKAREA 原子。
+    /// </summary>
+    /// <remarks>
+    /// _NET_WORKAREA, x, y, width, height CARDINAL[][4]/32
+    /// </remarks>
+    public X11DisplayAtom NetWorkarea => GetAtom("_NET_WORKAREA");
+
+    /// <summary>
+    /// _NET_SUPPORTING_WM_CHECK 原子。
+    /// </summary>
+    /// <remarks>
+    /// _NET_SUPPORTING_WM_CHECK, WINDOW/32
+    /// </remarks>
+    public X11DisplayAtom NetSupportingWmCheck => GetAtom("_NET_SUPPORTING_WM_CHECK");
+
+    /// <summary>
+    /// _NET_VIRTUAL_ROOTS 原子。
+    /// </summary>
+    /// <remarks>
+    /// _NET_VIRTUAL_ROOTS, WINDOW[]/32
+    /// </remarks>
+    public X11DisplayAtom NetVirtualRoots => GetAtom("_NET_VIRTUAL_ROOTS");
+
+    /// <summary>
+    /// _NET_DESKTOP_LAYOUT 原子。
+    /// </summary>
+    /// <remarks>
+    /// <code>
+    /// _NET_DESKTOP_LAYOUT, orientation, columns, rows, starting_corner CARDINAL[4]/32
+    ///
+    /// #define _NET_WM_ORIENTATION_HORZ 0
+    /// #define _NET_WM_ORIENTATION_VERT 1
+    ///
+    /// #define _NET_WM_TOPLEFT     0
+    /// #define _NET_WM_TOPRIGHT    1
+    /// #define _NET_WM_BOTTOMRIGHT 2
+    /// #define _NET_WM_BOTTOMLEFT  3
+    /// </code>
+    /// </remarks>
+    public X11DisplayAtom NetDesktopLayout => GetAtom("_NET_DESKTOP_LAYOUT");
+
+    /// <summary>
+    /// _NET_SHOWING_DESKTOP 原子。
+    /// </summary>
+    /// <remarks>
+    /// _NET_SHOWING_DESKTOP desktop, CARDINAL/32
+    /// </remarks>
+    public X11DisplayAtom NetShowingDesktop => GetAtom("_NET_SHOWING_DESKTOP");
+
+    #endregion
+
+    #region Other Root Window Messages
+
+    /// <summary>
+    /// _NET_CLOSE_WINDOW 原子。
+    /// </summary>
+    public X11DisplayAtom NetCloseWindow => GetAtom("_NET_CLOSE_WINDOW");
+
+    /// <summary>
+    /// _NET_MOVERESIZE_WINDOW 原子。
+    /// </summary>
+    public X11DisplayAtom NetMoveresizeWindow => GetAtom("_NET_MOVERESIZE_WINDOW");
+
+    /// <summary>
+    /// _NET_WM_MOVERESIZE 原子。
+    /// </summary>
+    public X11DisplayAtom NetWmMoveresize => GetAtom("_NET_WM_MOVERESIZE");
+
+    /// <summary>
+    /// _NET_RESTACK_WINDOW 原子。
+    /// </summary>
+    public X11DisplayAtom NetRestackWindow => GetAtom("_NET_RESTACK_WINDOW");
+
+    /// <summary>
+    /// _NET_REQUEST_FRAME_EXTENTS 原子。
+    /// </summary>
+    public X11DisplayAtom NetRequestFrameExtents => GetAtom("_NET_REQUEST_FRAME_EXTENTS");
+
+    #endregion
+
+    #region Application Window Properties
 
     /// <summary>
     /// _NET_WM_NAME 原子。
     /// </summary>
-    public X11DisplayAtom NetWmName => GetAtom(ref _netWmName, "_NET_WM_NAME");
+    /// <remarks>
+    /// _NET_WM_NAME, UTF8_STRING
+    /// </remarks>
+    public X11DisplayAtom NetWmName => GetAtom("_NET_WM_NAME");
 
-    private X11Atom _netWmState;
+    /// <summary>
+    /// _NET_WM_VISIBLE_NAME 原子。
+    /// </summary>
+    /// <remarks>
+    /// _NET_WM_VISIBLE_NAME, UTF8_STRING
+    /// </remarks>
+    public X11DisplayAtom NetWmVisibleName => GetAtom("_NET_WM_VISIBLE_NAME");
+
+    /// <summary>
+    /// _NET_WM_ICON_NAME 原子。
+    /// </summary>
+    /// <remarks>
+    /// _NET_WM_ICON_NAME, UTF8_STRING
+    /// </remarks>
+    public X11DisplayAtom NetWmIconName => GetAtom("_NET_WM_ICON_NAME");
+
+    /// <summary>
+    /// _NET_WM_VISIBLE_ICON_NAME 原子。
+    /// </summary>
+    /// <remarks>
+    /// _NET_WM_VISIBLE_ICON_NAME, UTF8_STRING
+    /// </remarks>
+    public X11DisplayAtom NetWmVisibleIconName => GetAtom("_NET_WM_VISIBLE_ICON_NAME");
+
+    /// <summary>
+    /// _NET_WM_DESKTOP 原子。
+    /// </summary>
+    /// <remarks>
+    /// _NET_WM_DESKTOP desktop, CARDINAL/32
+    /// </remarks>
+    public X11DisplayAtom NetWmDesktop => GetAtom("_NET_WM_DESKTOP");
+
+    /// <summary>
+    /// _NET_WM_WINDOW_TYPE 原子。
+    /// </summary>
+    /// <remarks>
+    /// _NET_WM_WINDOW_TYPE, ATOM[]/32
+    /// </remarks>
+    public X11DisplayAtom NetWmWindowType => GetAtom("_NET_WM_WINDOW_TYPE");
+
+    #region Window Type Atoms
+
+    /// <summary>
+    /// _NET_WM_WINDOW_TYPE_DESKTOP 原子。
+    /// </summary>
+    public X11DisplayAtom NetWmWindowTypeDesktop => GetAtom("_NET_WM_WINDOW_TYPE_DESKTOP");
+
+    /// <summary>
+    /// _NET_WM_WINDOW_TYPE_DOCK 原子。
+    /// </summary>
+    public X11DisplayAtom NetWmWindowTypeDock => GetAtom("_NET_WM_WINDOW_TYPE_DOCK");
+
+    /// <summary>
+    /// _NET_WM_WINDOW_TYPE_TOOLBAR 原子。
+    /// </summary>
+    public X11DisplayAtom NetWmWindowTypeToolbar => GetAtom("_NET_WM_WINDOW_TYPE_TOOLBAR");
+
+    /// <summary>
+    /// _NET_WM_WINDOW_TYPE_MENU 原子。
+    /// </summary>
+    public X11DisplayAtom NetWmWindowTypeMenu => GetAtom("_NET_WM_WINDOW_TYPE_MENU");
+
+    /// <summary>
+    /// _NET_WM_WINDOW_TYPE_UTILITY 原子。
+    /// </summary>
+    public X11DisplayAtom NetWmWindowTypeUtility => GetAtom("_NET_WM_WINDOW_TYPE_UTILITY");
+
+    /// <summary>
+    /// _NET_WM_WINDOW_TYPE_SPLASH 原子。
+    /// </summary>
+    public X11DisplayAtom NetWmWindowTypeSplash => GetAtom("_NET_WM_WINDOW_TYPE_SPLASH");
+
+    /// <summary>
+    /// _NET_WM_WINDOW_TYPE_DIALOG 原子。
+    /// </summary>
+    public X11DisplayAtom NetWmWindowTypeDialog => GetAtom("_NET_WM_WINDOW_TYPE_DIALOG");
+
+    /// <summary>
+    /// _NET_WM_WINDOW_TYPE_DROPDOWN_MENU 原子。
+    /// </summary>
+    public X11DisplayAtom NetWmWindowTypeDropdownMenu => GetAtom("_NET_WM_WINDOW_TYPE_DROPDOWN_MENU");
+
+    /// <summary>
+    /// _NET_WM_WINDOW_TYPE_POPUP_MENU 原子。
+    /// </summary>
+    public X11DisplayAtom NetWmWindowTypePopupMenu => GetAtom("_NET_WM_WINDOW_TYPE_POPUP_MENU");
+
+    /// <summary>
+    /// _NET_WM_WINDOW_TYPE_TOOLTIP 原子。
+    /// </summary>
+    public X11DisplayAtom NetWmWindowTypeTooltip => GetAtom("_NET_WM_WINDOW_TYPE_TOOLTIP");
+
+    /// <summary>
+    /// _NET_WM_WINDOW_TYPE_NOTIFICATION 原子。
+    /// </summary>
+    public X11DisplayAtom NetWmWindowTypeNotification => GetAtom("_NET_WM_WINDOW_TYPE_NOTIFICATION");
+
+    /// <summary>
+    /// _NET_WM_WINDOW_TYPE_COMBO 原子。
+    /// </summary>
+    public X11DisplayAtom NetWmWindowTypeCombo => GetAtom("_NET_WM_WINDOW_TYPE_COMBO");
+
+    /// <summary>
+    /// _NET_WM_WINDOW_TYPE_DND 原子。
+    /// </summary>
+    public X11DisplayAtom NetWmWindowTypeDnd => GetAtom("_NET_WM_WINDOW_TYPE_DND");
+
+    /// <summary>
+    /// _NET_WM_WINDOW_TYPE_NORMAL 原子。
+    /// </summary>
+    public X11DisplayAtom NetWmWindowTypeNormal => GetAtom("_NET_WM_WINDOW_TYPE_NORMAL");
+
+    #endregion
 
     /// <summary>
     /// _NET_WM_STATE 原子。
     /// </summary>
-    public X11DisplayAtom NetWmState => GetAtom(ref _netWmState, "_NET_WM_STATE");
+    /// <remarks>
+    /// _NET_WM_STATE, ATOM[]/32
+    /// </remarks>
+    public X11DisplayAtom NetWmState => GetAtom("_NET_WM_STATE");
 
-    private X11DisplayAtom GetAtom(ref X11Atom atom, string atomName)
+    #region Window State Atoms
+
+    /// <summary>
+    /// _NET_WM_STATE_MODAL 原子。
+    /// </summary>
+    public X11DisplayAtom NetWmStateModal => GetAtom("_NET_WM_STATE_MODAL");
+
+    /// <summary>
+    /// _NET_WM_STATE_STICKY 原子。
+    /// </summary>
+    public X11DisplayAtom NetWmStateSticky => GetAtom("_NET_WM_STATE_STICKY");
+
+    /// <summary>
+    /// _NET_WM_STATE_MAXIMIZED_VERT 原子。
+    /// </summary>
+    public X11DisplayAtom NetWmStateMaximizedVert => GetAtom("_NET_WM_STATE_MAXIMIZED_VERT");
+
+    /// <summary>
+    /// _NET_WM_STATE_MAXIMIZED_HORZ 原子。
+    /// </summary>
+    public X11DisplayAtom NetWmStateMaximizedHorz => GetAtom("_NET_WM_STATE_MAXIMIZED_HORZ");
+
+    /// <summary>
+    /// _NET_WM_STATE_SHADED 原子。
+    /// </summary>
+    public X11DisplayAtom NetWmStateShaded => GetAtom("_NET_WM_STATE_SHADED");
+
+    /// <summary>
+    /// _NET_WM_STATE_SKIP_TASKBAR 原子。
+    /// </summary>
+    public X11DisplayAtom NetWmStateSkipTaskbar => GetAtom("_NET_WM_STATE_SKIP_TASKBAR");
+
+    /// <summary>
+    /// _NET_WM_STATE_SKIP_PAGER 原子。
+    /// </summary>
+    public X11DisplayAtom NetWmStateSkipPager => GetAtom("_NET_WM_STATE_SKIP_PAGER");
+
+    /// <summary>
+    /// _NET_WM_STATE_HIDDEN 原子。
+    /// </summary>
+    public X11DisplayAtom NetWmStateHidden => GetAtom("_NET_WM_STATE_HIDDEN");
+
+    /// <summary>
+    /// _NET_WM_STATE_FULLSCREEN 原子。
+    /// </summary>
+    public X11DisplayAtom NetWmStateFullscreen => GetAtom("_NET_WM_STATE_FULLSCREEN");
+
+    /// <summary>
+    /// _NET_WM_STATE_ABOVE 原子。
+    /// </summary>
+    public X11DisplayAtom NetWmStateAbove => GetAtom("_NET_WM_STATE_ABOVE");
+
+    /// <summary>
+    /// _NET_WM_STATE_BELOW 原子。
+    /// </summary>
+    public X11DisplayAtom NetWmStateBelow => GetAtom("_NET_WM_STATE_BELOW");
+
+    /// <summary>
+    /// _NET_WM_STATE_DEMANDS_ATTENTION 原子。
+    /// </summary>
+    public X11DisplayAtom NetWmStateDemandsAttention => GetAtom("_NET_WM_STATE_DEMANDS_ATTENTION");
+
+    /// <summary>
+    /// _NET_WM_STATE_FOCUSED 原子。
+    /// </summary>
+    public X11DisplayAtom NetWmStateFocused => GetAtom("_NET_WM_STATE_FOCUSED");
+
+    #endregion
+
+    /// <summary>
+    /// _NET_WM_ALLOWED_ACTIONS 原子。
+    /// </summary>
+    /// <remarks>
+    /// _NET_WM_ALLOWED_ACTIONS, ATOM[]/32
+    /// </remarks>
+    public X11DisplayAtom NetWmAllowedActions => GetAtom("_NET_WM_ALLOWED_ACTIONS");
+
+    #region Allowed Actions Atoms
+
+    /// <summary>
+    /// _NET_WM_ACTION_MOVE 原子。
+    /// </summary>
+    public X11DisplayAtom NetWmActionMove => GetAtom("_NET_WM_ACTION_MOVE");
+
+    /// <summary>
+    /// _NET_WM_ACTION_RESIZE 原子。
+    /// </summary>
+    public X11DisplayAtom NetWmActionResize => GetAtom("_NET_WM_ACTION_RESIZE");
+
+    /// <summary>
+    /// _NET_WM_ACTION_MINIMIZE 原子。
+    /// </summary>
+    public X11DisplayAtom NetWmActionMinimize => GetAtom("_NET_WM_ACTION_MINIMIZE");
+
+    /// <summary>
+    /// _NET_WM_ACTION_SHADE 原子。
+    /// </summary>
+    public X11DisplayAtom NetWmActionShade => GetAtom("_NET_WM_ACTION_SHADE");
+
+    /// <summary>
+    /// _NET_WM_ACTION_STICK 原子。
+    /// </summary>
+    public X11DisplayAtom NetWmActionStick => GetAtom("_NET_WM_ACTION_STICK");
+
+    /// <summary>
+    /// _NET_WM_ACTION_MAXIMIZE_HORZ 原子。
+    /// </summary>
+    public X11DisplayAtom NetWmActionMaximizeHorz => GetAtom("_NET_WM_ACTION_MAXIMIZE_HORZ");
+
+    /// <summary>
+    /// _NET_WM_ACTION_MAXIMIZE_VERT 原子。
+    /// </summary>
+    public X11DisplayAtom NetWmActionMaximizeVert => GetAtom("_NET_WM_ACTION_MAXIMIZE_VERT");
+
+    /// <summary>
+    /// _NET_WM_ACTION_FULLSCREEN 原子。
+    /// </summary>
+    public X11DisplayAtom NetWmActionFullscreen => GetAtom("_NET_WM_ACTION_FULLSCREEN");
+
+    /// <summary>
+    /// _NET_WM_ACTION_CHANGE_DESKTOP 原子。
+    /// </summary>
+    public X11DisplayAtom NetWmActionChangeDesktop => GetAtom("_NET_WM_ACTION_CHANGE_DESKTOP");
+
+    /// <summary>
+    /// _NET_WM_ACTION_CLOSE 原子。
+    /// </summary>
+    public X11DisplayAtom NetWmActionClose => GetAtom("_NET_WM_ACTION_CLOSE");
+
+    /// <summary>
+    /// _NET_WM_ACTION_ABOVE 原子。
+    /// </summary>
+    public X11DisplayAtom NetWmActionAbove => GetAtom("_NET_WM_ACTION_ABOVE");
+
+    /// <summary>
+    /// _NET_WM_ACTION_BELOW 原子。
+    /// </summary>
+    public X11DisplayAtom NetWmActionBelow => GetAtom("_NET_WM_ACTION_BELOW");
+
+    #endregion
+
+    /// <summary>
+    /// _NET_WM_STRUT 原子。
+    /// </summary>
+    /// <remarks>
+    /// _NET_WM_STRUT left, right, top, bottom, CARDINAL[4]/32
+    /// </remarks>
+    public X11DisplayAtom NetWmStrut => GetAtom("_NET_WM_STRUT");
+
+    /// <summary>
+    /// _NET_WM_STRUT_PARTIAL 原子。
+    /// </summary>
+    /// <remarks>
+    /// _NET_WM_STRUT_PARTIAL left, right, top, bottom, left_start_y, left_end_y,
+    /// right_start_y, right_end_y, top_start_x, top_end_x, bottom_start_x, bottom_end_x,
+    /// CARDINAL[12]/32
+    /// </remarks>
+    public X11DisplayAtom NetWmStrutPartial => GetAtom("_NET_WM_STRUT_PARTIAL");
+
+    /// <summary>
+    /// _NET_WM_ICON_GEOMETRY 原子。
+    /// </summary>
+    /// <remarks>
+    /// _NET_WM_ICON_GEOMETRY x, y, width, height, CARDINAL[4]/32
+    /// </remarks>
+    public X11DisplayAtom NetWmIconGeometry => GetAtom("_NET_WM_ICON_GEOMETRY");
+
+    /// <summary>
+    /// _NET_WM_ICON 原子。
+    /// </summary>
+    /// <remarks>
+    /// _NET_WM_ICON, CARDINAL[][2+n]/32
+    /// </remarks>
+    public X11DisplayAtom NetWmIcon => GetAtom("_NET_WM_ICON");
+
+    /// <summary>
+    /// _NET_WM_PID 原子。
+    /// </summary>
+    /// <remarks>
+    /// _NET_WM_PID, CARDINAL/32
+    /// </remarks>
+    public X11DisplayAtom NetWmPid => GetAtom("_NET_WM_PID");
+
+    /// <summary>
+    /// _NET_WM_HANDLED_ICONS 原子。
+    /// </summary>
+    /// <remarks>
+    /// _NET_WM_HANDLED_ICONS, CARDINAL/32
+    /// </remarks>
+    public X11DisplayAtom NetWmHandledIcons => GetAtom("_NET_WM_HANDLED_ICONS");
+
+    /// <summary>
+    /// _NET_WM_USER_TIME 原子。
+    /// </summary>
+    /// <remarks>
+    /// _NET_WM_USER_TIME, CARDINAL/32
+    /// </remarks>
+    public X11DisplayAtom NetWmUserTime => GetAtom("_NET_WM_USER_TIME");
+
+    /// <summary>
+    /// _NET_WM_USER_TIME_WINDOW 原子。
+    /// </summary>
+    /// <remarks>
+    /// _NET_WM_USER_TIME_WINDOW, WINDOW/32
+    /// </remarks>
+    public X11DisplayAtom NetWmUserTimeWindow => GetAtom("_NET_WM_USER_TIME_WINDOW");
+
+    /// <summary>
+    /// _NET_FRAME_EXTENTS 原子。
+    /// </summary>
+    /// <remarks>
+    /// _NET_FRAME_EXTENTS, left, right, top, bottom, CARDINAL[4]/32
+    /// </remarks>
+    public X11DisplayAtom NetFrameExtents => GetAtom("_NET_FRAME_EXTENTS");
+
+    /// <summary>
+    /// _NET_WM_OPAQUE_REGION 原子。
+    /// </summary>
+    /// <remarks>
+    /// _NET_WM_OPAQUE_REGION, CARDINAL[][4]/32
+    /// </remarks>
+    public X11DisplayAtom NetWmOpaqueRegion => GetAtom("_NET_WM_OPAQUE_REGION");
+
+    /// <summary>
+    /// _NET_WM_BYPASS_COMPOSITOR 原子。
+    /// </summary>
+    /// <remarks>
+    /// _NET_WM_BYPASS_COMPOSITOR, CARDINAL/32
+    /// </remarks>
+    public X11DisplayAtom NetWmBypassCompositor => GetAtom("_NET_WM_BYPASS_COMPOSITOR");
+
+    /// <summary>
+    /// _NET_WM_WINDOW_OPACITY 原子。
+    /// </summary>
+    /// <remarks>
+    /// _NET_WM_WINDOW_OPACITY, CARDINAL/32
+    /// </remarks>
+    public X11DisplayAtom NetWmWindowOpacity => GetAtom("_NET_WM_WINDOW_OPACITY");
+
+    #endregion
+
+    #endregion
+
+    private X11DisplayAtom GetAtom(string atomName)
     {
-        if (atom == default)
-        {
-            var result = _display.InternAtom(atomName);
-            atom = result.Xid;
-            return result;
-        }
-
-        return atom.WithDisplay(_display);
+        var x11Atom = _atomCache.GetOrAdd(atomName, static (name, display) => XLib.XInternAtom(display, name, false), _display);
+        return x11Atom.WithDisplay(_display);
     }
 
     #endregion
